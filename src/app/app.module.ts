49358bd897219ff33ad1dd1ba3b5c09546aa93ca
Love Andersson <landersson@Loves-MacBook-Air.local>
2018-08-29 12:45:46 -0700

    minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { LoginComponent } from "./login/login.component";
 import { HttpClientModule } from "@angular/common/http";
-import { RegistrationComponent } from './registration/registration.component';
+import { RegistrationComponent } from "./registration/registration.component";
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegistrationComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
